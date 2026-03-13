97d26dcffddcacebbaa3c29bff500449a854a6dd
maddox-rage <57829840+maddox-rage@users.noreply.github.com>
2026-03-13 14:14:36 +0800

    Delete psycho/go-project directory

--- psycho/go-project/go.mod
+++
@@ -1,8 +0,0 @@
-module examplePractice
-
-go 1.25.7
-
-require (
-	github.com/jmoiron/sqlx v1.4.0 // indirect
-	github.com/lib/pq v1.11.2 // indirect
-)
--- psycho/go-project/internal/handlers/handlers.go
+++
@@ -1,9 +0,0 @@
-package handlers
-
-import "examplePractice/internal/database"
-
-type Handlers struct {
-	store *database.TaskStore
-}
-
-func NewHandlers
